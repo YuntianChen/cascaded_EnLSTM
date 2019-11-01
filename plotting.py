d49025fbf7b472522528083fe46d0e094ea19b9a
vixues <vixues@gmail.com>
2019-11-01 11:13:17 +0800

    adjust figure size

--- plotting.py
+++ plotting.py
@@ -5,7 +5,7 @@ import os
 
 def draw_comparing_diagram(pred, pred_std, target, ylabel=None, title=None, save_path=None):
     x = np.arange(len(target))
-    plt.figure(figsize=(18, 5))
+    plt.figure(figsize=(30, 5))
     plt.plot(target, label='target', color='black', alpha=0.4)
     plt.errorbar(x, pred, yerr=pred_std, color='red', alpha=0.7)
     plt.title('')
